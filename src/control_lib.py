61f310dc4af718b7ca417a0f97d66eb165c03fe5
Lucas Zuñiga <lucas.ziga@uc.cl>
2025-07-18 01:15:33 -0400

    avance_close_loop

--- src/control_lib.py
+++ src/control_lib.py
@@ -5,7 +5,7 @@ Libreria para el control de actuadores del robot IRB.
 ##### Librerias #####
 
 from machine import Pin , PWM
-from utime import sleep
+from time import sleep, sleep_ms, ticks_us
 
 
 ##### Definicion de pines #####
@@ -19,13 +19,19 @@ d1_stby = Pin(3, Pin.OUT)
 d1_ina1 = Pin(2,Pin.OUT)
 d1_ina2 = Pin(1, Pin.OUT)
 d1_pwma = PWM(Pin(0))
+## Encoder 1
+m1_enc1 = Pin(7, Pin.IN)
+m1_enc2 = Pin(8, Pin.IN)
 
 # Driver 2
 d2_stby = Pin(11, Pin.OUT)
-##   Motor 1
+##   Motor 2
 d2_ina1 = Pin(6,Pin.OUT)
 d2_ina2 = Pin(5, Pin.OUT)
 d2_pwma = PWM(Pin(4))
+## Encoder 2
+m2_enc1 = Pin(9, Pin.IN)
+m2_enc2 = Pin(10, Pin.IN)
 ##   Rodillo
 d2_inb1 = Pin(12,Pin.OUT)
 d2_inb2 = Pin(13, Pin.OUT)
@@ -34,13 +40,6 @@ d2_pwmb = PWM(Pin(14))
 # Solenoide
 sol = Pin(15, Pin.OUT)
 
-# Encoders
-## Motor 1
-m1_enc1 = Pin(7, Pin.IN)
-m1_enc2 = Pin(8, Pin.IN)
-## Motor 2
-m2_enc1 = Pin(9, Pin.IN)
-m2_enc2 = Pin(10, Pin.IN)
 
 ##### Definicion de funciones ####
 
@@ -96,28 +95,80 @@ def solenoid_on():
 def solenoid_off():
     sol.value(0)
 
-##### main #####
+##### Main #####
 
-d1_pwma.freq(1000)
-d1_stby.value(1)  # Enable the motor driver 1
+d1_pwma.freq(1000)      ### Averiguar sobre esta linea ###
+d1_stby.value(1)        # Enable the motor driver 1
 d2_pwma.freq(1000)
-d2_stby.value(1)  # Enable the motor driver 2
+d2_stby.value(1)        # Enable the motor driver 2
 
-led.toggle()
+led.toggle()            # Encender el LED para indicar que el programa ha iniciado
+
+# while True:
+#     txt = input()
+#     if txt == "s":
+#         stop()
+#         break
+    
+#     elif txt == "f":
+#         duty_cycle = min(int(input("Ingrese el ciclo de trabajo (0-100): ")), 100)
+#         forward(duty_cycle)
+        
+#     elif txt == "b":
+#         duty_cycle = min(int(input("Ingrese el ciclo de trabajo (0-100): ")), 100)
+#         backward(duty_cycle)
+
+# Close Loop Motor 1
+vel_ref = 10 # Velocidad de referencia [RPM]
+vel_lectura = 0
+
+## Parametros Controlador
+Kp = 0.5 # Ganancia proporcional
+Ki = 0 # Ganancia integral
+Kd = 0 # Ganancia derivativa
+
+error = 0
+error_prev = 0
+integral = 0
+duty = 30
+
+delay = 1
+
+prev_enc_value = 1
+tiempo_entre_enc = 0
 
 while True:
-    txt = input()
-    if txt == "s":
-        stop()
-        break
-    
-    elif txt == "f":
-        duty_cycle = min(int(input("Ingrese el ciclo de trabajo (0-100): ")), 100)
-        forward(duty_cycle)
+    print(m2_enc1.value())
+    if m2_enc1.value():
+        led.toggle()
+    # Calculo vel actual
+    if not prev_enc_value and m2_enc1.value():
+        vel_lectura = 60/tiempo_entre_enc               # [RPM]
+        tiempo_entre_enc = 0
         
-    elif txt == "b":
-        duty_cycle = min(int(input("Ingrese el ciclo de trabajo (0-100): ")), 100)
-        backward(duty_cycle)
+    else:
+        tiempo_entre_enc += delay
+    
+    # Calculo parametros ajuste
+    error =  vel_ref - vel_lectura
+    derivativo = (error - error_prev)/delay
+    integral += error
+
+    duty += Kp*error + Ki*integral + Kd*derivativo      # Calcula el porcentaje de PWM necesario
+    duty = min(max(duty, -100), 100)                    # Se asegura de no sobrepasar el 100% de la PWM
+    
+    # print(duty)
+    if duty >= 0:
+        # RotateCW(duty, d1_ina1, d1_ina2, d1_pwma)
+        # print("rota")
+        pass
+    else:
+        pass
+        # RotateCCW(-duty, d1_ina1, d1_ina2, d1_pwma)
+        
+    error_prev = error
+    sleep(delay)
+
 
 stop()
-led.toggle()
+led.toggle()            # Apagar el LED para indicar que el programa ha terminado
