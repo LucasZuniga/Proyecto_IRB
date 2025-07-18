42275138009165911bf21e0701a69bc4fcdc0065
Lucas Zuñiga <lucas.ziga@uc.cl>
2025-07-17 23:48:33 -0400

    todos_los_pines

--- src/control_lib.py
+++ src/control_lib.py
@@ -2,29 +2,47 @@
 Libreria para el control de actuadores del robot IRB.
 
 """
+##### Librerias #####
+
 from machine import Pin , PWM
 from utime import sleep
 
-# driver 1
-d1_ina1 = Pin(2,Pin.OUT)
-d1_ina2 = Pin(1, Pin.OUT)
-d1_stby = Pin(3, Pin.OUT)
-d1_pwma = PWM(Pin(0))
 
-# driver 2
-d2_ina1 = Pin(6,Pin.OUT)
-d2_ina2 = Pin(5, Pin.OUT)
-d2_stby = Pin(11, Pin.OUT)
-d2_pwma = PWM(Pin(4))
+##### Definicion de pines #####
 
+# LED
 led = Pin("LED", Pin.OUT)
 
-d1_pwma.freq(1000)
-d1_stby.value(1)  # Enable the motor driver 1
-d2_pwma.freq(1000)
-d2_stby.value(1)  # Enable the motor driver 2
+# Driver 1
+d1_stby = Pin(3, Pin.OUT)
+##  Motor 1
+d1_ina1 = Pin(2,Pin.OUT)
+d1_ina2 = Pin(1, Pin.OUT)
+d1_pwma = PWM(Pin(0))
 
-led.toggle()
+# Driver 2
+d2_stby = Pin(11, Pin.OUT)
+##   Motor 1
+d2_ina1 = Pin(6,Pin.OUT)
+d2_ina2 = Pin(5, Pin.OUT)
+d2_pwma = PWM(Pin(4))
+##   Rodillo
+d2_inb1 = Pin(12,Pin.OUT)
+d2_inb2 = Pin(13, Pin.OUT)
+d2_pwmb = PWM(Pin(14))
+
+# Solenoide
+sol = Pin(15, Pin.OUT)
+
+# Encoders
+## Motor 1
+m1_enc1 = Pin(7, Pin.IN)
+m1_enc2 = Pin(8, Pin.IN)
+## Motor 2
+m2_enc1 = Pin(9, Pin.IN)
+m2_enc2 = Pin(10, Pin.IN)
+
+##### Definicion de funciones ####
 
 def RotateCW(duty, m1, m2, pwm):
     m1.value(1)
@@ -54,6 +72,38 @@ def forward(duty):
 def stop():
     StopMotor(d1_ina1, d1_ina2, d1_pwma)
     StopMotor(d2_ina1, d2_ina2, d2_pwma)
+    
+def rodillo_in(duty):
+    d2_inb1.value(1)
+    d2_inb2.value(0)
+    duty_16 = int((duty*65535)/100)
+    d2_pwmb.duty_u16(duty_16)
+
+def rodillo_out(duty):
+    d2_inb1.value(0)
+    d2_inb2.value(1)
+    duty_16 = int((duty*65535)/100)
+    d2_pwmb.duty_u16(duty_16)
+    
+def rodillo_stop():
+    d2_inb1.value(0)
+    d2_inb2.value(0)
+    d2_pwmb.duty_u16(0)
+
+def solenoid_on():
+    sol.value(1)
+
+def solenoid_off():
+    sol.value(0)
+
+##### main #####
+
+d1_pwma.freq(1000)
+d1_stby.value(1)  # Enable the motor driver 1
+d2_pwma.freq(1000)
+d2_stby.value(1)  # Enable the motor driver 2
+
+led.toggle()
 
 while True:
     txt = input()
@@ -62,13 +112,12 @@ while True:
         break
     
     elif txt == "f":
-        duty_cycle = int(input("Ingrese el ciclo de trabajo (0-100): "))
+        duty_cycle = min(int(input("Ingrese el ciclo de trabajo (0-100): ")), 100)
         forward(duty_cycle)
         
     elif txt == "b":
-        duty_cycle = int(input("Ingrese el ciclo de trabajo (0-100): "))
+        duty_cycle = min(int(input("Ingrese el ciclo de trabajo (0-100): ")), 100)
         backward(duty_cycle)
-
 
 stop()
 led.toggle()
