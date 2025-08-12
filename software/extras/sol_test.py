2c4543d6545a00676da9c6a0658a60bb4829013c
Lucas Zuñiga <lucas.ziga@uc.cl>
2025-08-12 12:21:56 -0400

    sol_test

---
+++ software/extras/sol_test.py
@@ -0,0 +1,55 @@
+##### Librerias ####
+
+import network
+import socket
+import _thread
+import rp2
+import sys
+import uasyncio
+from machine import Pin, PWM
+from time import sleep, sleep_ms, ticks_us, time_ns
+
+##### Definicion de pines #####
+
+# LEDS
+led = Pin("LED", Pin.OUT)
+led_verde = Pin(20, Pin.OUT)
+led_amarillo = Pin(19, Pin.OUT)
+led_azul = Pin(18, Pin.OUT)
+
+led_azul.off()
+led_verde.off()
+led_amarillo.off()
+led.off()
+
+# # Driver 1
+# d1_stby = Pin(3, Pin.OUT)
+# ##  Motor 1
+# d1_ina1 = Pin(2,Pin.OUT)
+# d1_ina2 = Pin(1, Pin.OUT)
+# d1_pwma = PWM(Pin(0))
+# ## Encoder 1
+# m1_enc1 = Pin(7, Pin.IN, Pin.PULL_UP)
+# m1_enc2 = Pin(8, Pin.IN, Pin.PULL_UP)
+
+# # Driver 2
+# d2_stby = Pin(11, Pin.OUT)
+# ##   Motor 2
+# d2_ina1 = Pin(6,Pin.OUT)
+# d2_ina2 = Pin(5, Pin.OUT)
+# d2_pwma = PWM(Pin(4))
+# ## Encoder 2
+# m2_enc1 = Pin(9, Pin.IN, Pin.PULL_UP)
+# m2_enc2 = Pin(10, Pin.IN, Pin.PULL_UP)
+# ##   Rodillo
+# d2_inb1 = Pin(12,Pin.OUT)
+# d2_inb2 = Pin(13, Pin.OUT)
+# d2_pwmb = PWM(Pin(14))
+
+# Solenoide
+sol = Pin(15, Pin.OUT)
+
+while True:
+    solenoide = int(input("1 encender o 0 apagar"))
+    # sol.value(solenoide)
+    led.value(0)
